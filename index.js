b794ebc65af7699c72ced9327643ad49dcd5f127
Pablo Vinicius <pablovinicius4568@gmail.com>
2024-04-19 06:13:15 -0300

    Criação do Model Usuario e execucao do 1 sync

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const conn = require("./db/conn");
+const Usuario = require("./models/Usuario")
+
+conn
+  .sync()
+  .then(() => {
+    console.log("Conectado ao banco de dados com sucesso!");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro: " + err);
+  });
